64cb733c8fbc54e9c4ab4a4d4d1165a82d67a9de
Siddharth Sahu <46642995+siddharth8763@users.noreply.github.com>
2025-05-13 11:52:26 +0530

    Add files via upload

---
+++ advanced.py
@@ -0,0 +1,57 @@
+# # sort (inplace) and sorted (creates a new list)
+# arr=[1,2,3,4,5]
+# # arr.sort(reverse=True)
+# print(arr)
+# arr2=sorted(arr,reverse=True)
+# print(arr2)
+
+##############################################################################
+
+# fill arrays with zeros
+# arr = [0] * 5
+# print(arr)
+
+##############################################################################
+
+##appendeing
+# arr = [0] * 5
+# print(arr)
+# arr2=[1,1,1,1,1]
+# print(arr+arr2)
+
+##########################################################
+
+#slicing
+# arr=[1,2,3,4,5,6,7,8,9,10]
+# print(arr[1::1])
+# print(arr[1::])
+# print(arr[::1])
+# print(arr[::2])
+# print(arr[2::])
+# print(arr[2:8:3])
+# print(arr[::-1])
+
+#################################################
+
+#deep copy and shallow copy
+# arr=[1,2,3]
+# arr1_deepcopy = arr.copy()
+# arr1_deepcopy1 = list(arr)
+# arr1_deepcopy2 = arr[:]
+# arr1_shallowcopy = arr
+# arr1_deepcopy.reverse()
+# arr1_shallowcopy.reverse()
+# print(arr1_deepcopy)    
+# print(arr)
+#print(arr1_deepcopy1)
+#print(arr1_deepcopy2)
+
+#################################################
+
+# list comprehension
+# arr = [1,2,3,4,5]
+# squaredArray = [i*i for i in arr]
+# print(arr)
+# print(squaredArray)
+
+##################################################
