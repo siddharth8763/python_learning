01cf7ef3794744ef876d3236e2eaa63c6c55e8df
Siddharth Sahu <46642995+siddharth8763@users.noreply.github.com>
2025-07-22 21:54:21 +0530

    Generators

--- advanced.py
+++ advanced.py
@@ -639,3 +639,87 @@
 # say_hello()    
 
 ################################################################ 
+
+# def mygenerator():
+#     yield 1
+#     yield 2
+#     yield 3
+
+# g=mygenerator()
+# value=next(g)
+# print(value)
+# value=next(g)
+# print(value)
+# value=next(g)
+# print(value)
+# value=next(g)
+# print(value) # stop iteration error
+
+###############################################################
+
+# def mygenerator():
+#     yield 1
+#     yield 2
+#     yield 3
+
+# print(sum(mygenerator()))  # Output: 6
+
+#############################################################
+
+# def countdown(num):
+#     print('Starting')
+#     while num > 0:
+#         yield num
+#         num-=1
+# cd=countdown(4)
+# value=next(cd)
+# print(value)
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
+# print(next(cd))
+
+################################################################
+
+# import sys
+# # This code demonstrates the difference in memory usage between a list and a generator
+# # not optimised
+# def firstn(n):
+#     nums=[]
+#     num=0
+#     while num <n:
+#         nums.append(num)
+#         num+= 1
+#     return nums
+
+# # optimised
+# def firstn_generator(n):
+#     num = 0
+#     while num<n:
+#         yield num
+#         num += 1
+# print(sys.getsizeof(firstn(1000000)))
+# print(sys.getsizeof(firstn_generator (1000000)))
+
+#######################################################
+
+# def fibonacci(limit):
+# #01123 5 8 13
+#     a, b = 0,1
+#     while a<limit:
+#         yield a
+#         a, b = b, a+b
+
+# fib=fibonacci(30)
+# for i in fib:
+#     print(i)
+
+#########################################################
+
+# import sys
+# mygenerator=(i for i in range(10) if i % 2 == 0)
+# print(sys.getsizeof(mygenerator))
+# mylist=[i for i in range(10) if 1 % 2 == 0]
+# print(sys.getsizeof(mylist))
+
+####################################
