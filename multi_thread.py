8cb56614c094023bf17ec13700ac1d4e29275040
Siddharth Sahu <46642995+siddharth8763@users.noreply.github.com>
2025-10-31 22:36:21 +0530

    multi thread changes

--- multi_thread.py
+++ multi_thread.py
@@ -87,43 +87,78 @@ import psutil  # Import psutil for resource monitoring
 
 # using Lock prevent race condition
 
-database_value = 0
+# database_value = 0
 
-def increase(lock):
-    global database_value
+# def increase(lock):
+#     global database_value
 
-    # lock.acquire()
-    # local_copy = database_value
-    # local_copy += 1
-    # time.sleep(0.1)
-    # database_value = local_copy
-    # lock.release()
+#     # lock.acquire()
+#     # local_copy = database_value
+#     # local_copy += 1
+#     # time.sleep(0.1)
+#     # database_value = local_copy
+#     # lock.release()
 
-    # with context manager
-    with lock:
-        local_copy = database_value
-        local_copy += 1
-        time.sleep(0.1)
-        database_value = local_copy
+#     # with context manager
+#     with lock:
+#         local_copy = database_value
+#         local_copy += 1
+#         time.sleep(0.1)
+#         database_value = local_copy
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    lock = Lock()
+#     lock = Lock()
 
-    print(f"Start value: {database_value}")
+#     print(f"Start value: {database_value}")
 
-    thread1 = Thread(target=increase, args=(lock,))
-    thread2 = Thread(target=increase, args=(lock,))
+#     thread1 = Thread(target=increase, args=(lock,))
+#     thread2 = Thread(target=increase, args=(lock,))
 
-    thread1.start()
-    thread2.start()
+#     thread1.start()
+#     thread2.start()
 
-    thread1.join()
-    thread2.join()
+#     thread1.join()
+#     thread2.join()
 
-    print(f"End value: {database_value}")   
+#     print(f"End value: {database_value}")   
 
-    print("End main")
+#     print("End main")
 
 ########################################################
 
+#queue
+# These are threadsafe FIFO data structures that can be used to share data between threads.
+# This example demonstrates the use of a queue to distribute work among multiple threads.
+# lock is used to synchronize access to shared resources (printing to console in this case).
+
+# from threading import Thread, Lock, current_thread
+# from queue import Queue
+# import time
+
+# def worker(q,lock):
+#     while True:
+#         value = q.get()
+#         with lock:
+#             print(f"Thread {current_thread().name} processing value: {value}")
+#         q.task_done()
+
+# if __name__ == '__main__':
+#     q= Queue()
+#     lock = Lock()
+
+#     num_threads = 10
+
+#     for i in range(num_threads):
+#         thread=Thread(target=worker, args=(q,lock))
+#         thread.daemon=True
+#         thread.start()
+
+#     for i in range(1,21):
+#         q.put(i)
+
+#     q.join()
+
+#     print("end main")
+
+###################################################################
