c780ba1f4cac19bf4ae66e188f0870a1121a94ab
Siddharth Sahu <46642995+siddharth8763@users.noreply.github.com>
2025-11-01 22:13:51 +0530

    function changes more

--- function.py
+++ function.py
@@ -141,3 +141,48 @@ my_list3 = [10, 20, 30]
 fool3(my_list3)
 # the original list is not modified because a_list is reassigned to a new list
 print(my_list3)
+
+print("----------------------------------")
+
+zeros=[0]*10
+zero_ones=[0,1]*10
+print(zeros)
+print(zero_ones)
+
+print("----------------------------------")
+
+numbers_list_for_unpacking=[1,2,3,4,5,6,7,8,9,10]
+numbers_list_for_unpacking=(1,2,3,4,5,6,7,8,9,10)
+# it will always unpack to a list irrespective of list or tuple
+a,b,*rest,last=numbers_list_for_unpacking
+a1,b1,*rest1,last1=numbers_list_for_unpacking
+print(f"a: {a}, b: {b}, rest: {rest}, last: {last}")
+print(f"a: {a1}, b: {b1}, rest: {rest1}, last: {last1}")
+
+print("----------------------------------")
+
+# merging
+
+list1=[1,2,3]
+list2=[4,5,6]
+tuple1=(7,8,9)
+tuple2=(10,11,12)
+set1={13,14,15}
+dict1={'a':1, 'b':2}
+dict2={'c':3, 'd':4}
+
+merged_list=[*list1, *list2]
+merged_tuple=(*tuple1, *tuple2)
+merged_mix=(*list1, *tuple2)
+merged_mix2=[*tuple1, *list2]
+merged_mix3={*set1, *list1}
+merged_dict={**dict1, **dict2}
+# not possible
+# merged_dict1={**dict2, *list1}  
+# order matters for overlapping keys
+print("merged_list:", merged_list)
+print("merged_tuple:", merged_tuple)
+print("merged_mix:", merged_mix)
+print("merged_mix2:", merged_mix2)
+print("merged_mix2:", merged_mix3)
+print("merged_dict:", merged_dict)
